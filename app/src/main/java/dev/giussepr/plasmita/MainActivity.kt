fbfc0339add5d82a16ce8f75799a3b483bbdfc9a
giussepr <giussepr@gmail.com>
2025-06-26 10:59:59 -0500

    Create activity view model and update Ui state

--- app/src/main/java/dev/giussepr/plasmita/MainActivity.kt
+++ app/src/main/java/dev/giussepr/plasmita/MainActivity.kt
@@ -4,26 +4,50 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.lifecycle.compose.collectAsStateWithLifecycle
 import dev.giussepr.plasmita.ui.theme.PlasmitaTheme
 
 class MainActivity : ComponentActivity() {
+
+    private val viewModel: MainActivityViewModel by lazy {
+        MainActivityViewModel()
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
+
+            val uiState by viewModel.uiState.collectAsStateWithLifecycle()
+
             PlasmitaTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .padding(innerPadding),
+                        verticalArrangement = Arrangement.Center,
+                        horizontalAlignment = Alignment.CenterHorizontally
+                    ) {
+                        StatelessCounter(
+                            counter = uiState.counter,
+                            onIncrement = {
+                                viewModel.onIntent(ViewIntent.Increment)
+                            }
+                        )
+                    }
                 }
             }
         }
@@ -31,17 +55,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PlasmitaTheme {
-        Greeting("Android")
+fun StatelessCounter(modifier: Modifier = Modifier, counter: Int, onIncrement: () -> Unit) {
+    Button(onClick = onIncrement) {
+        Text(
+            text = "Counter: $counter",
+            modifier = modifier
+        )
     }
 }
--- app/src/main/java/dev/giussepr/plasmita/MainActivityViewModel.kt
+++ app/src/main/java/dev/giussepr/plasmita/MainActivityViewModel.kt
@@ -1,4 +1,31 @@
 package dev.giussepr.plasmita
 
-class MainActivityViewModel {
+import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.flow.update
+
+class MainActivityViewModel: ViewModel() {
+
+    private val _uiState = MutableStateFlow(UiState())
+    val uiState = _uiState.asStateFlow()
+
+    fun onIntent(intent: ViewIntent) {
+        when (intent) {
+            is ViewIntent.Increment -> {
+                _uiState.update {
+                    it.copy(counter = it.counter + 1)
+                }
+            }
+        }
+    }
+
+}
+
+data class UiState(
+    val counter: Int = 0
+)
+
+sealed interface ViewIntent {
+    data object Increment : ViewIntent
 }
